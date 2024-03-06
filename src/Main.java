896d091b90130e460527767ebe5030aa6fcc9bf6
Akvilė <sutkaityte.akvile@gmail.com>
2024-03-06 23:27:52 +0200

    ka

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ha!");
-        System.out.println("Ha!");
-        System.out.println("Ha!");
+        System.out.println("ka!");
+
     }
 }
